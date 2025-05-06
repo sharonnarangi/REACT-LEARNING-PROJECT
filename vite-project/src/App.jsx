fd1377b7aa9f1a36bd20218e4d24436e315a8ee0
sharonnarangi <sharonnarangi@gmail.com>
2025-05-06 18:00:51 +0300

    Chore: Install React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-center text-purple-700">
+        Law Firm App Setup Successful!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
